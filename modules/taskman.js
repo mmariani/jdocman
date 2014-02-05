351e21b0a65eb4d433b2f4b501ec64d43ea9e2ee
Marco Mariani <marco.mariani@nexedi.com>
2014-02-05 16:49:42 +0100

    removed task-util, small cleanup

--- modules/taskman.js
+++ modules/taskman.js
@@ -1,8 +1,47 @@
 /*jslint indent: 2, nomen: true, vars: true, browser: true */
-/*global alert, $, Logger, RSVP, task_util, dav_storage, Handlebars, jiodate, moment, i18n, jIO, task_data, Blob, complex_queries  */
+/*global alert, $, Logger, RSVP, dav_storage, Handlebars, jiodate, moment, i18n, jIO, task_data, Blob, complex_queries, Sanitize  */
 
 $(document).on('mobileinit', function () {
   "use strict";
+
+
+  /**
+   * Remove accents and convert to lower case
+   *
+   * @param {String} s the string to convert.
+   * @return {String} the converted string.
+   */
+  function accentFoldLC(s) {
+    var map = [
+        [new RegExp('[àáâãäå]', 'gi'), 'a'],
+        [new RegExp('æ', 'gi'), 'ae'],
+        [new RegExp('ç', 'gi'), 'c'],
+        [new RegExp('[èéêë]', 'gi'), 'e'],
+        [new RegExp('[ìíîï]', 'gi'), 'i'],
+        [new RegExp('ñ', 'gi'), 'n'],
+        [new RegExp('[òóôõö]', 'gi'), 'o'],
+        [new RegExp('œ', 'gi'), 'oe'],
+        [new RegExp('[ùúûü]', 'gi'), 'u'],
+        [new RegExp('[ýÿ]', 'gi'), 'y']
+      ];
+
+    if (!s) {
+      return s;
+    }
+
+    map.forEach(function (o) {
+      var rep = function (match) {
+        if (match.toUpperCase() === match) {
+          return o[1].toUpperCase();
+        }
+        return o[1];
+      };
+      s = s.replace(o[0], rep);
+    });
+    return s.toLowerCase();
+  }
+
+
 
   /**
    * Defines a schema of search keys for the task queries,
@@ -17,18 +56,18 @@ $(document).on('mobileinit', function () {
     match_lookup: {
       translatedStateMatch: function (object_value, value) {
         var translated_object_value = i18n.t(object_value);
-        return RSVP.resolve(task_util.accentFoldLC(translated_object_value) ===
-                            task_util.accentFoldLC(value));
+        return RSVP.resolve(accentFoldLC(translated_object_value) ===
+                            accentFoldLC(value));
       }
     },
     key_set: {
       title: {
         read_from: 'title',
-        cast_to: task_util.accentFoldLC
+        cast_to: accentFoldLC
       },
       description: {
         read_from: 'description',
-        cast_to: task_util.accentFoldLC
+        cast_to: accentFoldLC
       },
       start: {
         read_from: 'start',
@@ -43,17 +82,21 @@ $(document).on('mobileinit', function () {
         equal_match: 'translatedStateMatch'
       }
     }
-  };
+  },
+    //
+    // Data passed around for page changes -- we cannot use URL parameters with appcache
+    // Waiting for better parameter support in JQM 1.5 (http://jquerymobile.com/roadmap/)
+    //
+    page_params = {},
+    default_storage_id = 'default_storage';
 
-  var default_storage_id = 'default_storage';
 
 
-
-  /***********************************
-   *                                 *
-   * Function definitions start here *
-   *                                 *
-   ***********************************/
+  /****************************************
+   *                                      *
+   * Most function definitions start here *
+   *                                      *
+   ****************************************/
 
 
   function getSelectedStorage() {
@@ -89,15 +132,31 @@ $(document).on('mobileinit', function () {
 
 
   /**
-   * This function must be used as a then parameter if you don't want to manage
-   * errors. It changes the promise to the fulfillment channel with no fulfillment
-   * value.
-   *
-   *     doSomething().fail(ignoreError).then(...);
+   * Update a <select> element's selected option,
+   * then activates the jquery mobile event to refresh UI
    */
-  function ignoreError() {
-    Logger.error('Error ignored!');
-    // no error propagated here
+  function jqmSetSelected(el, value) {
+    var $select = $(el);
+
+    /*jslint unparam: true*/
+    $select.children().each(function (i, op) {
+      if (op.getAttribute('value') === value) {
+        op.setAttribute('selected', 'selected');
+      }
+    });
+    /*jslint unparam: false*/
+
+    $select.selectmenu('refresh');
+  }
+
+
+  /**
+   * Retrieves the text typed by the user for searching.
+   *
+   * @return {String} the search string
+   */
+  function getSearchString() {
+    return $('#search-tasks').val().trim();
   }
 
 
@@ -479,16 +538,6 @@ $(document).on('mobileinit', function () {
 
 
   /**
-   * Retrieves the text a user is using for searches.
-   *
-   * @return {String} the search string
-   */
-  function getSearchString() {
-    return $('#search-tasks').val().trim();
-  }
-
-
-  /**
    * Display (or refresh) a list of tasks in the current page,
    * performing a search if there is search input.
    * Translation is applied after rendering the template.
@@ -541,7 +590,7 @@ $(document).on('mobileinit', function () {
         applyTranslation();
 
         // select the current language on the menu
-        task_util.jqmSetSelected('#translate', i18n.lng());
+        jqmSetSelected('#translate', i18n.lng());
       });
   }
 
@@ -605,70 +654,105 @@ $(document).on('mobileinit', function () {
 
 
 
-
-  /***************************
-   *                         *
-   * Application starts here *
-   *                         *
-   ***************************/
-
-  var input_timer = null,
-    //
-    // Data passed around for page changes -- we cannot use URL parameters with appcache
-    // Waiting for better parameter support in JQM 1.5 (http://jquerymobile.com/roadmap/)
-    page_params = {};
-
-  if (!getSelectedStorage()) {
-    setSelectedStorage(default_storage_id);
-  }
-
-  $('.initHandler').removeClass('initHandler');
-
-  $.mobile.selectmenu.prototype.options.nativeMenu = false;
-
-  if (!hasHTML5DatePicker()) {
-    $.datepicker.setDefaults({dateFormat: 'yy-mm-dd'});
-  }
-
-  task_util.registerHandlebarsHelpers();
-
-  Logger.useDefaults();   // log to console
-
-  // DEBUG for development, WARN for production
-  Logger.setLevel(Logger.DEBUG);
+  /***********************************
+   *                                 *
+   * Handlebars helpers registration *
+   *                                 *
+   ***********************************/
 
   /**
-   * Set up the translations for i18next.
-   * This might trigger a 404 for an unsupported locale
-   * before falling back, i.e. en-US -> en
-   * Also applies translations to the current page as soon
-   * as the data is ready.
-   * For the options, see http://i18next.com/pages/doc_init.html
+   * Clean up HTML before display for security reasons
+   * (see https://github.com/gbirke/Sanitize.js)
+   * Also, truncates the resulting string if it's too long.
+   * This may leave bad formatting in 'relaxed' mode,
+   * but we are using the strictest mode, which only
+   * preserves the text content.
+   *
+   * @param {String} html The insecure string to sanitize.
+   * @param {String} maxsize The length to trim the string at.
+   * @return {String} The safe (will not be escaped) string to render in HTML.
    */
-  $.i18n.init({
-    detectLngQS: 'lang',
-    fallbackLng: 'en',
-    ns: 'translation',
-    resGetPath: 'i18n/__lng__/__ns__.json',
-    preload: ['en', 'fr', 'zh']
-  }, applyTranslation);
+  Handlebars.registerHelper('sanitize', function (html, maxsize) {
+    // The Sanitize module only works on DOM nodes, so we create one from the string...
+    var node = $('<div>' + html + '</div>'),
+      s = new Sanitize(),
+      clean_fragment = s.clean_node(node[0]),
+      // ...take the resulting fragment...
+      tmpdiv = document.createElement('div'),
+      text = '';
+
+    // ...and wrap the fragment around a node...
+    tmpdiv.appendChild(clean_fragment);
+    // ...only to access its innerHTML property.
+    // It would be simpler if Sanitize took a string.
+
+    text = tmpdiv.innerHTML;
+
+    if (maxsize && text.length > maxsize) {
+      // truncate string to word boundary
+      text = text.substr(0, maxsize - 1);
+      if (text.indexOf(' ') !== -1) {
+        // single huge word, cut it
+        text = text.substr(0, text.lastIndexOf(' '));
+      }
+      text = text + '&hellip;';
+    }
+
+    return new Handlebars.SafeString(text);
+  });
 
 
   /**
-   * Attempt to make jqm dialogs transparent.
-   * XXX this does not work if the page has been changed before
-   * opening the dialog.
+   * Display date strings or objects as yyyy-mm-dd - see https://xkcd.com/1179/
+   * (takes timezone into account)
+   *
+   * @param {String} date The date string (or Date object) to display.
+   * @return {String} The safe (will not be escaped) string to render in HTML.
+   * Escaped or not, it doesn't make a real difference here.
    */
-  /*jslint unparam: true*/
-  $(document).on('pagebeforeshow', 'div[data-role="dialog"]', function (e, ui) {
-    ui.prevPage.addClass('ui-dialog-background ');
+  Handlebars.registerHelper('asYMD', function (date) {
+    if (date) {
+      return new Handlebars.SafeString(moment(date).format('YYYY-MM-DD'));
+    }
+    return '';
   });
 
-  $(document).on('pagehide', 'div[data-role="dialog"]', function (e, ui) {
-    $('.ui-dialog-background ').removeClass('ui-dialog-background ');
-  });
-  /*jslint unparam: false*/
 
+  /**
+   * Make translations accessible from within Handlebars templates
+   */
+  Handlebars.registerHelper('t', function (i18n_key) {
+    return new Handlebars.SafeString(i18n.t(i18n_key));
+  });
+
+
+  /**
+   * Add value comparisions, see also
+   * http://github.com/assemble/handlebars-helpers/blob/master/lib/helpers/helpers-comparisons.js
+   * The operator must be quoted:
+   *
+   * {{#ifCond v1 '===' v2}}
+   * ...
+   * {{/ifCond}}
+   */
+  Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    switch (operator) {
+    case '!==':
+      return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+    case '===':
+      return (v1 === v2) ? options.fn(this) : options.inverse(this);
+    case '<':
+      return (v1 < v2) ? options.fn(this) : options.inverse(this);
+    case '<=':
+      return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+    case '>':
+      return (v1 > v2) ? options.fn(this) : options.inverse(this);
+    case '>=':
+      return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+    }
+  });
 
 
 
@@ -793,6 +877,8 @@ $(document).on('mobileinit', function () {
   });
 
 
+  var _input_timer = null;
+
   /**
    * Perform a search and update the task list.
    * A timer is used to avoid querying for each character.
@@ -811,14 +897,14 @@ $(document).on('mobileinit', function () {
     }
 
     jioConnect().then(function (jio) {
-      if (input_timer) {
-        window.clearTimeout(input_timer);
-        input_timer = null;
+      if (_input_timer) {
+        window.clearTimeout(_input_timer);
+        _input_timer = null;
       }
-      input_timer = window.setTimeout(function () {
+      _input_timer = window.setTimeout(function () {
         updateTaskList(jio); // XXX errors from this promise are not propagated
                              // and would have to be handled separately.
-        input_timer = 0;
+        _input_timer = 0;
       }, 500);
     }).fail(displayError);
   });
@@ -833,7 +919,6 @@ $(document).on('mobileinit', function () {
    */
   $(document).on('click', '.task-details-link', function () {
     page_params = {task_id: $(this).data('jio-id')};
-    Logger.info('task target id', page_params.task_id);
     $.mobile.navigate('task-details.html');
   });
 
@@ -878,8 +963,8 @@ $(document).on('mobileinit', function () {
           $('#task-details-container')
             .html(template({task: task_resp.data, projects: projects, states: states, dateinput_type: dateinput_type}))
             .trigger('create');
-          task_util.jqmSetSelected('#task-project', task_resp.data.project);
-          task_util.jqmSetSelected('#task-state', task_resp.data.state);
+          jqmSetSelected('#task-project', task_resp.data.project);
+          jqmSetSelected('#task-state', task_resp.data.state);
           // XXX if the project does not exist anymore, the first one is selected
           applyTranslation();
         });
@@ -1201,6 +1286,65 @@ $(document).on('mobileinit', function () {
         });
     }).fail(displayError);
   });
+
+
+
+  /***************************
+   *                         *
+   * Application starts here *
+   *                         *
+   ***************************/
+
+
+  if (!getSelectedStorage()) {
+    setSelectedStorage(default_storage_id);
+  }
+
+  // avoid FOUC (http://en.wikipedia.org/wiki/Flash_of_unstyled_content)
+  $('.initHandler').removeClass('initHandler');
+
+  $.mobile.selectmenu.prototype.options.nativeMenu = false;
+
+  if (!hasHTML5DatePicker()) {
+    $.datepicker.setDefaults({dateFormat: 'yy-mm-dd'});
+  }
+
+  Logger.useDefaults();   // log to console
+
+  // DEBUG for development, WARN for production
+  Logger.setLevel(Logger.DEBUG);
+
+  /**
+   * Set up the translations for i18next.
+   * This might trigger a 404 for an unsupported locale
+   * before falling back, i.e. en-US -> en
+   * Also applies translations to the current page as soon
+   * as the data is ready.
+   * For the options, see http://i18next.com/pages/doc_init.html
+   */
+  $.i18n.init({
+    detectLngQS: 'lang',
+    fallbackLng: 'en',
+    ns: 'translation',
+    resGetPath: 'i18n/__lng__/__ns__.json',
+    preload: ['en', 'fr', 'zh']
+  }, applyTranslation);
+
+
+  /**
+   * Attempt to make jqm dialogs transparent.
+   * XXX this does not work if the page has been changed before
+   * opening the dialog.
+   */
+  /*jslint unparam: true*/
+  $(document).on('pagebeforeshow', 'div[data-role="dialog"]', function (e, ui) {
+    ui.prevPage.addClass('ui-dialog-background ');
+  });
+
+  $(document).on('pagehide', 'div[data-role="dialog"]', function (e, ui) {
+    $('.ui-dialog-background ').removeClass('ui-dialog-background ');
+  });
+  /*jslint unparam: false*/
 
 });
 
